8c92b5d983631057263d8737f4ff070c627abea2
ilavryk <illia.lavryk@intellias.com>
2021-11-08 16:03:37 +0200

    small refactoring, code enhancement

--- src/main/java/com/t360/filtering/core/QueryNodeImpl.java
+++ src/main/java/com/t360/filtering/core/QueryNodeImpl.java
@@ -7,7 +7,6 @@ import java.sql.JDBCType;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -31,45 +30,38 @@ public class QueryNodeImpl<T> implements QueryNode<T> {
             int index = 1;
             for (PredicateValueDescriptor valHolder : collectPredicates()) {
                 Class<?> valueClass = valHolder.getValue().getClass();
-
-                if (valueClass.equals(BigDecimal.class)) {
-                    ps.setBigDecimal(index++, (BigDecimal) valHolder.getValue());
-                } else if (valueClass.equals(String.class)) {
-                    ps.setString(index++, (String) valHolder.getValue());
-                } else if (valueClass.equals(Boolean.class)) {
-                    ps.setBoolean(index++, (Boolean) valHolder.getValue());
-                } else if (valueClass.equals(Byte.class)) {
-                    ps.setByte(index++, (Byte) valHolder.getValue());
-                } else if (valueClass.equals(Short.class)) {
-                    ps.setShort(index++, (Short) valHolder.getValue());
-                } else if (valueClass.equals(Integer.class)) {
-                    ps.setInt(index++, (Integer) valHolder.getValue());
-                } else if (valueClass.equals(Long.class)) {
-                    ps.setLong(index++, (Long) valHolder.getValue());
-                } else if (valHolder.getValue() instanceof Collection) {
-                    // todo check if array works on other jdbc vendors
-//                    Object[] array = ((Collection<?>) valHolder.getValue()).toArray();
-//                    String typeName = resolveJdbcType(array[0]);
-//                    Array sqlArray = ps.getConnection().createArrayOf(typeName, array);
-//                    ps.setArray(index++, sqlArray);
-                    Iterator<?> it = ((Collection<?>) valHolder.getValue()).iterator();
-                    while (it.hasNext()) {
-                        if (valHolder.getFieldType().equals(BigDecimal.class)) {
-                            ps.setBigDecimal(index++, (BigDecimal) it.next());
-                        } else if (valHolder.getFieldType().equals(String.class)) {
-                            ps.setString(index++, (String) it.next());
-                        } else if (valHolder.getFieldType().equals(Integer.class)) {
-                            ps.setInt(index++, (Integer) it.next());
-                        } else if (valHolder.getFieldType().equals(Long.class)) {
-                            ps.setLong(index++, (Long) it.next());
-                        }
-                    }
-                }
+                if (valHolder.getValue() instanceof Collection) {
+                    for (Object o : (Collection<?>) valHolder.getValue())
+                        resolveAndSetField(ps, index++, valHolder.getFieldType(), o);
+                } else resolveAndSetField(ps, index++, valueClass, valHolder.getValue());
             }
         } catch (SQLException e) {
             e.printStackTrace();
             throw new IllegalStateException("Cant correctly configure prepare statement: SQL State " + e.getSQLState());
         }
+    }
+
+    private void resolveAndSetField(PreparedStatement ps, int index, Class<?> valueClass, Object val) throws SQLException {
+        if (valueClass.equals(BigDecimal.class)) {
+            ps.setBigDecimal(index++, (BigDecimal) val);
+        } else if (valueClass.equals(String.class)) {
+            ps.setString(index++, (String) val);
+        } else if (valueClass.equals(Boolean.class)) {
+            ps.setBoolean(index++, (Boolean) val);
+        } else if (valueClass.equals(Byte.class)) {
+            ps.setByte(index++, (Byte) val);
+        } else if (valueClass.equals(Short.class)) {
+            ps.setShort(index++, (Short) val);
+        } else if (valueClass.equals(Integer.class)) {
+            ps.setInt(index++, (Integer) val);
+        } else if (valueClass.equals(Long.class)) {
+            ps.setLong(index++, (Long) val);
+        }
+        // todo check if array works on other jdbc vendors
+//         Object[] array = ((Collection<?>) valHolder.getValue()).toArray();
+//         String typeName = resolveJdbcType(array[0]);
+//         Array sqlArray = ps.getConnection().createArrayOf(typeName, array);
+//         ps.setArray(index++, sqlArray);
     }
 
     private String resolveJdbcType(Object obj) {
